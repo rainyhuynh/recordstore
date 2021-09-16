10515604968240654492565060b9c45f1ba37eaa
rainy huynh <rainyhuynh83@gmail.com>
2021-09-16 08:13:01 +0700

    done init API backend and initing front end

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  namespace :api do 
+    namespace :v1 do   
+      resources :artists do 
+        resources :records
+      end
+    end
+  end
+
+  post 'refresh', controller: :refresh, action: :create 
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
+  
+end
